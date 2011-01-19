f7510a16ea7dd6403dd086b88106aaee4ad5605f
Daniel Janke <danijank@uni-koblenz.de>
2011-01-19 11:32:10 +0000

    Modified interface IncidenceClass.

--- src/de/uni_koblenz/jgralab/schema/IncidenceClass.java
+++ src/de/uni_koblenz/jgralab/schema/IncidenceClass.java
@@ -37,17 +37,103 @@ import de.uni_koblenz.jgralab.Incidence;
 
 public interface IncidenceClass {
 
+	public static final String DEFAULTEDGECLASS_NAME = "Incidence";
+
+	/**
+	 * adds a superclass to the list of superclasses
+	 * 
+	 * @param superClass
+	 *            the edge class to be added to the list of superclasses if an
+	 *            attribute name exists in superClass and in this class
+	 * 
+	 */
+	public void addSuperClass(IncidenceClass superClass);
+
+	/**
+	 * Lists all direct subclasses of this element.
+	 * 
+	 * <p>
+	 * <b>Pattern:</b>
+	 * <code>subClasses = incidences.getDirectSubClasses();</code>
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Preconditions:</b> none
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Postconditions:</b>
+	 * <ul>
+	 * <li><code>subClasses != null</code></li>
+	 * <li><code>subClasses.size() >= 0</code></li>
+	 * <li><code>subClasses</code> holds all of <code>attrElement´s</code>
+	 * direct subclasses</li>
+	 * <li><code>subClasses</code> does not hold any of
+	 * <code>attrElement´s</code> inherited subclasses</li>
+	 * </ul>
+	 * </p>
+	 * 
+	 * @return a Set of all direct subclasses of this element
+	 */
+	public Set<IncidenceClass> getDirectSubClasses();
+
+	/**
+	 * Returns all direct superclasses of this element.
+	 * 
+	 * <p>
+	 * <b>Note:</b> Each instance of a subclass of <code>IncidenceClass</code>
+	 * has one default direct superclass. Please consult the specifications of
+	 * the used subclass for details.
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Pattern:</b>
+	 * <code>superClasses = incidence.getDirectSuperClasses();</code>
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Preconditions:</b> none
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Postconditions:</b>
+	 * <ul>
+	 * <li><code>superClasses != null</code></li>
+	 * <li><code>superClasses.size() >= 0</code></li>
+	 * <li><code>superClasses</code> holds all of <code>incidence´s</code>
+	 * direct superclasses (including the default superclass)</li>
+	 * <li><code>superClasses</code> does not hold any of
+	 * <code>attrElement´s</code> inherited superclasses
+	 * </ul>
+	 * </p>
+	 * 
+	 * @return a Set of all direct superclasses of this element
+	 */
+	public Set<IncidenceClass> getDirectSuperClasses();
+
 	/**
 	 * @return the upper multiplicity, i.e. the maximal number of edges
-	 *         connected to the vertex at the opposite end
+	 *         connected to the vertex
 	 */
-	public int getMax();
+	public int getMaxEdgesAtVertex();
+
+	/**
+	 * @return the upper multiplicity, i.e. the maximal number of vertices
+	 *         connected to the edge
+	 */
+	public int getMaxVerticesAtEdge();
 
 	/**
 	 * @return the lower multiplicity, i.e. the minimal number of edges
 	 *         connected to the vertex at the opposite end
 	 */
-	public int getMin();
+	public int getMinEdgesAtVertex();
+
+	/**
+	 * @return the lower multiplicity, i.e. the minimal number of edges
+	 *         connected to the vertex at the opposite end
+	 */
+	public int getMinVerticesAtEdge();
 
 	/**
 	 * @return the direction of this incidenceclass - either Vertex (from edge
@@ -62,41 +148,86 @@ public interface IncidenceClass {
 	public String getRolename();
 
 	/**
-	 * @return the type of this IncidenceClass, NONE for a normal edge end,
+	 * States if this IncidenceClass is abstract. Abstract elements can´t have
+	 * instances.
+	 * 
+	 * <p>
+	 * <b>Pattern:</b> <code>isAbstract = incidenceClass.isAbstract();</code>
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Preconditions:</b> none
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Postconditions:</b> <code>isAbstract</code> is:
+	 * <ul>
+	 * <li><code>true</code> if <code>incidenceClass</code> is abstract and
+	 * therefore may not have any instances</li>
+	 * <li>otherwise <code>false</code>
+	 * </ul>
+	 * 
+	 * @return <code>true</code>, if the element is abstract , otherwise
+	 *         <code>false</code>
+	 */
+	public boolean isAbstract();
+
+	/**
+	 * @return the type of this IncidenceClass, EDGE for a normal edge end,
 	 *         AGGREGATION for an aggregation end and COMPOSITION for a
 	 *         composition end
 	 */
-	public AggregationKind getAggregationKind();
+	public IncidenceType getIncidentType();
 
 	/**
-	 * sets the type of this IncidenceClass, NONE for a normal edge end,
+	 * sets the type of this IncidenceClass, EDGE for a normal edge end,
 	 * AGGREGATION for an aggregation end and COMPOSITION for a composition end
 	 */
-	public void setAggregationKind(AggregationKind kind);
+	public void setIncidenceType(IncidenceType kind);
 
 	/**
+	 * TODO
+	 * 
 	 * @return the set of IncidenceClasses which are subsetted (i.e.
 	 *         specialized) by this IncidenceClass
 	 */
+	@Deprecated
 	public Set<IncidenceClass> getSubsettedIncidenceClasses();
 
 	/**
+	 * TODO
+	 * 
 	 * @return the set of IncidenceClasses which are directly subsetted (i.e.
 	 *         specialized) by this IncidenceClass
 	 */
+	@Deprecated
 	public Set<IncidenceClass> getOwnSubsettedIncidenceClasses();
 
 	/**
+	 * TODO
+	 * 
 	 * @return the set of IncidenceClasses which are redefined (i.e. specialized
 	 *         and overwritten) by this IncidenceClass
 	 */
+	@Deprecated
 	public Set<IncidenceClass> getRedefinedIncidenceClasses();
 
 	/**
+	 * TODO
+	 * 
 	 * @return the set of IncidenceClasses which are directly redefined (i.e.
 	 *         specialized and overwritten) by this IncidenceClass
 	 */
+	@Deprecated
 	public Set<IncidenceClass> getOwnRedefinedIncidenceClasses();
+
+	public Set<IncidenceClass> getHidingEndsAtEdge();
+
+	public Set<IncidenceClass> getHiddenEndsAtEdge();
+
+	public Set<IncidenceClass> getHidingEndsAtVertex();
+
+	public Set<IncidenceClass> getHiddenEndsAtVertex();
 
 	/**
 	 * @return the VertexClass this IncidenceClass is connected to
@@ -114,19 +245,21 @@ public interface IncidenceClass {
 	public Set<String> getAllRoles();
 
 	/**
+	 * TODO
+	 * 
 	 * @return the set of roles which are redefined by this IncidenceClass
 	 */
 	public Set<String> getRedefinedRoles();
 
 	/**
-	 * Marks a role which is already subsetted by this IncidenceClass as
+	 * TODO Marks a role which is already subsetted by this IncidenceClass as
 	 * redefined.
 	 */
 	public void addRedefinedRole(String rolename);
 
 	/**
-	 * Marks a set of roles which are already subsetted by this IncidenceClass
-	 * as redefined.
+	 * TODO Marks a set of roles which are already subsetted by this
+	 * IncidenceClass as redefined.
 	 */
 	public void addRedefinedRoles(Set<String> rolenames);
 
