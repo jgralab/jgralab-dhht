20cf3ae56627cdf10c8b7649d69cf652bcd1412c
Daniel Bildhauer <dbildh@uni-koblenz.de>
2011-02-23 14:46:10 +0100

    minfix

--- src/de/uni_koblenz/jgralab/TypedElement.java
+++ src/de/uni_koblenz/jgralab/TypedElement.java
@@ -5,7 +5,7 @@ import de.uni_koblenz.jgralab.schema.Schema;
 import de.uni_koblenz.jgralab.schema.TypedElementClass;
 
 public interface TypedElement<ConcreteMetaClass extends TypedElementClass<ConcreteMetaClass, ConcreteInterface>, ConcreteInterface extends TypedElement<ConcreteMetaClass, ConcreteInterface>>
-                 extends Comparable<TypedElement<ConcreteMetaClass, ConcreteInterface>> {
+                 extends Comparable<ConcreteInterface> {
 
 	/**
 	 * Returns the m1-class of this {@link AttributedElement}.
