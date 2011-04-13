ab530e96fafdb0aa470084d4ec1ca98c7a83ec8a
Daniel Janke <danijank@uni-koblenz.de>
2011-04-13 14:35:25 +0200

    Removed warnings.

--- src/de/uni_koblenz/jgralab/impl/PartialGraphImpl.java
+++ src/de/uni_koblenz/jgralab/impl/PartialGraphImpl.java
@@ -1,18 +1,6 @@
 package de.uni_koblenz.jgralab.impl;
 
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Map;
-
 import de.uni_koblenz.jgralab.Edge;
-import de.uni_koblenz.jgralab.Graph;
-import de.uni_koblenz.jgralab.GraphIO;
-import de.uni_koblenz.jgralab.GraphIOException;
-import de.uni_koblenz.jgralab.JGraLabList;
-import de.uni_koblenz.jgralab.JGraLabMap;
-import de.uni_koblenz.jgralab.JGraLabSet;
-import de.uni_koblenz.jgralab.NoSuchAttributeException;
-import de.uni_koblenz.jgralab.Record;
 import de.uni_koblenz.jgralab.Vertex;
 import de.uni_koblenz.jgralab.schema.GraphClass;
 import de.uni_koblenz.jgralab.schema.Schema;
@@ -20,72 +8,70 @@ import de.uni_koblenz.jgralab.schema.Schema;
 /**
  * Implements a partial Graph as member of a distributed complete one
  * 
- * TODO:
- * - Implement add and delete edge/vertex operations with correct linking on 
- *   next/previous vertex in global graph
- * -   
+ * TODO: - Implement add and delete edge/vertex operations with correct linking
+ * on next/previous vertex in global graph -
  * 
  * @author dbildh
- *
+ * 
  */
 public abstract class PartialGraphImpl extends CompleteOrPartialGraphImpl {
-	
+
 	/* holds the graph this partial graph belongs to */
 	protected GraphBaseImpl completeGraph;
-	
 
 	boolean loading = false;
 
 	protected PartialGraphImpl(GraphClass cls, GraphBaseImpl completeGraph) {
 		super(cls);
 		this.completeGraph = completeGraph;
-		id = ((CompleteGraphImpl)completeGraph.getCompleteGraph()).allocateFreePartialGraphId();
+		id = ((CompleteGraphImpl) completeGraph.getCompleteGraph())
+				.allocateFreePartialGraphId();
 	}
 
 	@Override
 	public GraphBaseImpl getCompleteGraph() {
 		return completeGraph;
 	}
-	
+
 	/**
 	 * Changes this graph's version. graphModified() is called whenever the
 	 * graph is changed, all changes like adding, creating and reordering of
 	 * edges and vertices or changes of attributes of the graph, an edge or a
-	 * vertex are treated as a change. 
+	 * vertex are treated as a change.
 	 */
+	@Override
 	public void graphModified() {
 		graphVersion++;
 		completeGraph.graphModified();
 	}
-	
-	
+
 	/**
 	 * Changes the vertex sequence version of this graph. Should be called
 	 * whenever the vertex list of this graph is changed, for instance by
 	 * creation and deletion or reordering of vertices.
 	 */
+	@Override
 	protected void vertexListModified() {
 		vertexListVersion++;
 		graphVersion++;
 		completeGraph.vertexListModified();
 	}
-	
+
 	/**
 	 * Changes the vertex sequence version of this graph. Should be called
 	 * whenever the vertex list of this graph is changed, for instance by
 	 * creation and deletion or reordering of vertices.
 	 */
+	@Override
 	protected void edgeListModified() {
 		edgeListVersion++;
 		graphVersion++;
 		completeGraph.edgeListModified();
 	}
 
-	
-
 	/*
-	 *TODO: Should return true, if e is part of this graph or
-	 * any of its partial graphs
+	 * TODO: Should return true, if e is part of this graph or any of its
+	 * partial graphs
 	 */
 	@Override
 	public boolean containsEdge(Edge e) {
@@ -93,11 +79,9 @@ public abstract class PartialGraphImpl extends CompleteOrPartialGraphImpl {
 				&& containsEdgeId(((EdgeImpl) e).id);
 	}
 
-
-
 	/*
-	 * TODO: Should return true, if v is part of this graph or
-	 * any of its partial graphs
+	 * TODO: Should return true, if v is part of this graph or any of its
+	 * partial graphs
 	 */
 	@Override
 	public boolean containsVertex(Vertex v) {
@@ -107,10 +91,9 @@ public abstract class PartialGraphImpl extends CompleteOrPartialGraphImpl {
 				&& (vertex[((VertexImpl) v).id] == v);
 	}
 
-
 	@Override
 	public void deleteVertex(Vertex v) {
-		//TODO
+		// TODO
 	}
 
 	@Override
@@ -120,11 +103,9 @@ public abstract class PartialGraphImpl extends CompleteOrPartialGraphImpl {
 		edgeListModified();
 	}
 
-
-
 	private void internalDeleteEdge(Edge e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -132,11 +113,9 @@ public abstract class PartialGraphImpl extends CompleteOrPartialGraphImpl {
 		return getCompleteGraph().getUid();
 	}
 
-
 	@Override
 	public Schema getSchema() {
 		return schema;
 	}
-
 
 }
--- src/de/uni_koblenz/jgralab/impl/PartialSubordinateGraphImpl.java
+++ src/de/uni_koblenz/jgralab/impl/PartialSubordinateGraphImpl.java
@@ -3,27 +3,27 @@ package de.uni_koblenz.jgralab.impl;
 import de.uni_koblenz.jgralab.GraphElement;
 import de.uni_koblenz.jgralab.schema.GraphClass;
 
-
-
 /**
- * Subordinate graph which is distributed on an other station of a network and thus
- * is also a partial graph.
+ * Subordinate graph which is distributed on an other station of a network and
+ * thus is also a partial graph.
+ * 
  * @author dbildh
  * 
- * TODO: Implement class according to combined functionality of Partial and subordinate graph
+ *         TODO: Implement class according to combined functionality of Partial
+ *         and subordinate graph
  * 
- * Implement creation method in GraphElement to create a subordinate graph on another host
- *
+ *         Implement creation method in GraphElement to create a subordinate
+ *         graph on another host
+ * 
  */
 public abstract class PartialSubordinateGraphImpl extends PartialGraphImpl {
 
-	private GraphElement containingElement;
-	
-	protected PartialSubordinateGraphImpl(String id, GraphClass cls, GraphElement elem) {
-		super(cls, (GraphBaseImpl)elem.getGraph());
+	private final GraphElement<?, ?, ?> containingElement;
+
+	protected PartialSubordinateGraphImpl(String id, GraphClass cls,
+			GraphElement<?, ?, ?> elem) {
+		super(cls, (GraphBaseImpl) elem.getGraph());
 		containingElement = elem;
 	}
-	
-	
 
 }
