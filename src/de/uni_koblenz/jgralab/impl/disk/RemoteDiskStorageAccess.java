18b771c904115a0cfc55147dbf0a30f421211337
Daniel Bildhauer <dbildh@uni-koblenz.de>
2011-09-13 14:40:38 +0200

    Added remote exceptions to methods of RemoteDiskStorageAccess

--- src/de/uni_koblenz/jgralab/impl/disk/RemoteDiskStorageAccess.java
+++ src/de/uni_koblenz/jgralab/impl/disk/RemoteDiskStorageAccess.java
@@ -2,120 +2,145 @@ package de.uni_koblenz.jgralab.impl.disk;
 
 import java.rmi.Remote;
 
-
-
 /**
- * Remote interface of DiskStorage used to access properties of the stored elements
+ * Remote interface of DiskStorage used to access properties of the stored
+ * elements
+ * 
  * @author dbildh
- *
+ * 
  */
 public interface RemoteDiskStorageAccess extends Remote {
 
-	/* ========================================================
-	 * Methods to access hierarchy 
-	 * ======================================================== */
-	
-	public long getSigmaIdOfVertexId(int localElementId);
-	
-	public void setSigmaIdOfVertexId(int localElementId, long globalSigmaId);
+	/*
+	 * ======================================================== Methods to
+	 * access hierarchy ========================================================
+	 */
 
-	public int getKappaOfVertexId(int localIncidenceId);
+	public long getSigmaIdOfVertexId(int localElementId)
+			throws RuntimeException;
 
-	public void setKappaOfVertexId(int localIncidenceId, int kappa);
-	
-	public long getSigmaIdOfEdgeId(int localElementId);
-	
-	public void setSigmaIdOfEdgeId(int localElementId, long globalSigmaId);
+	public void setSigmaIdOfVertexId(int localElementId, long globalSigmaId)
+			throws RuntimeException;
 
-	public int getKappaOfEdgeId(int localIncidenceId);
+	public int getKappaOfVertexId(int localIncidenceId) throws RuntimeException;
 
-	public void setKappaOfEdgeId(int localIncidenceId, int kappa);
-	
-	
-	/* ========================================================
-	 * Methods to access vertices
-	 * ======================================================== */
-	
-	public int getVertexTypeId(int localVertexId);
-	
-	
-	public long getNextVertexId(int localVertexId);
-	
-	public void setNextVertexId(int localVertexId, long nextVertexId);
-	
-	public long getPreviousVertexId(int localVertexId);
-	
-	public void setPreviousVertexId(int localVertexId, long previousVertexId);
-	
-	/* ========================================================
-	 * Methods to access edges
-	 * ======================================================== */
-	
-	public int getEdgeTypeId(int localEdgeId);
-	
-	public long getNextEdgeId(int localEdgeId);
-	
-	public void setNextEdgeId(int localEdgeId, long nextEdgeId);
-	
-	public long getPreviousEdgeId(int localEdgeId);
-	
-	public void setPreviousEdgeId(int localEdgeId, long previousEdgeId);
-	
-	
-	/* ========================================================
-	 * Methods to access incidences 
-	 * ======================================================== */
-	
-	public long getFirstIncidenceIdAtVertexId(int localElementId);
-	
-	public void setFirstIncidenceIdAtVertexId(int localElementId, long l);
-	
-	public long getLastIncidenceIdAtVertexId(int localElementId);
+	public void setKappaOfVertexId(int localIncidenceId, int kappa)
+			throws RuntimeException;
 
-	public void setLastIncidenceIdAtVertexId(int localElementId, long id);
-		
-	public long getNextIncidenceIdAtVertexId(int convertToLocalId);
+	public long getSigmaIdOfEdgeId(int localElementId) throws RuntimeException;
 
-	public long getPreviousIncidenceIdAtVertexId(int convertToLocalId);
-	
-	public void setNextIncidenceAtVertexId(int localIncidenceId, long nextIncId);
-	
-	public void setPreviousIncidenceAtVertexId(int localIncidenceId, long previousIncId);
-	
-	public long getConnectedVertexId(int incidenceId);
-	
-	
-	public long getIncidenceListVersionOfVertexId(int elementId);
-	
-	public void increaseIncidenceListVersionOfVertexId(int elementId);
-	
-	
-	public long getFirstIncidenceIdAtEdgeId(int localElementId);
-	
-	public void setFirstIncidenceIdAtEdgeId(int localElementId, long l);
-	
-	public long getLastIncidenceIdAtEdgeId(int localElementId);
+	public void setSigmaIdOfEdgeId(int localElementId, long globalSigmaId)
+			throws RuntimeException;
 
-	public void setLastIncidenceIdAtEdgeId(int localElementId, long id);
-	
-	public long getNextIncidenceIdAtEdgeId(int convertToLocalId);
-	
-	public long getPreviousIncidenceIdAtEdgeId(int convertToLocalId);
-		
-	public void setNextIncidenceAtEdgeId(int localIncidenceId, long nextIncId);
-	
-	public void setPreviousIncidenceAtEdgeId(int localIncidenceId, long previousIncId);
+	public int getKappaOfEdgeId(int localIncidenceId) throws RuntimeException;
 
-	public long getConnectedEdgeId(int incidenceId);
-	
-	public long getIncidenceListVersionOfEdgeId(int elementId);
-	
-	public void increaseIncidenceListVersionOfEdgeId(int elementId);
-	
-	
+	public void setKappaOfEdgeId(int localIncidenceId, int kappa)
+			throws RuntimeException;
 
-	public int getIncidenceTypeId(int localIncidenceId);
-	
+	/*
+	 * ======================================================== Methods to
+	 * access vertices ========================================================
+	 */
 
+	public int getVertexTypeId(int localVertexId) throws RuntimeException;
+
+	public long getNextVertexId(int localVertexId) throws RuntimeException;
+
+	public void setNextVertexId(int localVertexId, long nextVertexId)
+			throws RuntimeException;
+
+	public long getPreviousVertexId(int localVertexId) throws RuntimeException;
+
+	public void setPreviousVertexId(int localVertexId, long previousVertexId)
+			throws RuntimeException;
+
+	/*
+	 * ======================================================== Methods to
+	 * access edges ========================================================
+	 */
+
+	public int getEdgeTypeId(int localEdgeId) throws RuntimeException;
+
+	public long getNextEdgeId(int localEdgeId) throws RuntimeException;
+
+	public void setNextEdgeId(int localEdgeId, long nextEdgeId)
+			throws RuntimeException;
+
+	public long getPreviousEdgeId(int localEdgeId) throws RuntimeException;
+
+	public void setPreviousEdgeId(int localEdgeId, long previousEdgeId)
+			throws RuntimeException;
+
+	/*
+	 * ======================================================== Methods to
+	 * access incidences
+	 * ========================================================
+	 */
+
+	public long getFirstIncidenceIdAtVertexId(int localElementId)
+			throws RuntimeException;
+
+	public void setFirstIncidenceIdAtVertexId(int localElementId, long id)
+			throws RuntimeException;
+
+	public long getLastIncidenceIdAtVertexId(int localElementId)
+			throws RuntimeException;
+
+	public void setLastIncidenceIdAtVertexId(int localElementId, long id)
+			throws RuntimeException;
+
+	public long getNextIncidenceIdAtVertexId(int convertToLocalId)
+			throws RuntimeException;
+
+	public long getPreviousIncidenceIdAtVertexId(int convertToLocalId)
+			throws RuntimeException;
+
+	public void setNextIncidenceAtVertexId(int localIncidenceId, long nextIncId)
+			throws RuntimeException;
+
+	public void setPreviousIncidenceAtVertexId(int localIncidenceId,
+			long previousIncId) throws RuntimeException;
+
+	public long getConnectedVertexId(int incidenceId) throws RuntimeException;
+
+	public long getIncidenceListVersionOfVertexId(int elementId)
+			throws RuntimeException;
+
+	public void increaseIncidenceListVersionOfVertexId(int elementId)
+			throws RuntimeException;
+
+	public long getFirstIncidenceIdAtEdgeId(int localElementId)
+			throws RuntimeException;
+
+	public void setFirstIncidenceIdAtEdgeId(int localElementId, long l)
+			throws RuntimeException;
+
+	public long getLastIncidenceIdAtEdgeId(int localElementId)
+			throws RuntimeException;
+
+	public void setLastIncidenceIdAtEdgeId(int localElementId, long id)
+			throws RuntimeException;
+
+	public long getNextIncidenceIdAtEdgeId(int convertToLocalId)
+			throws RuntimeException;
+
+	public long getPreviousIncidenceIdAtEdgeId(int convertToLocalId)
+			throws RuntimeException;
+
+	public void setNextIncidenceAtEdgeId(int localIncidenceId, long nextIncId)
+			throws RuntimeException;
+
+	public void setPreviousIncidenceAtEdgeId(int localIncidenceId,
+			long previousIncId) throws RuntimeException;
+
+	public long getConnectedEdgeId(int incidenceId) throws RuntimeException;
+
+	public long getIncidenceListVersionOfEdgeId(int elementId)
+			throws RuntimeException;
+
+	public void increaseIncidenceListVersionOfEdgeId(int elementId)
+			throws RuntimeException;
+
+	public int getIncidenceTypeId(int localIncidenceId) throws RuntimeException;
 
 }
