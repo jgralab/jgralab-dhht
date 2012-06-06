f3295c16f9eb18b67b332a8eccb6643220d9adec
Jon <jtheegarten@uni-koblenz.de>
2012-06-06 15:57:50 +0200

    Some test improvements Temporarily removed a line in Serializer until a fix is found.

--- testit/de/uni_koblenz/jgralabtest/greql2/DHHTParserTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/DHHTParserTest.java
@@ -10,13 +10,20 @@ import de.uni_koblenz.jgralab.greql2.exception.ParsingException;
 import de.uni_koblenz.jgralab.greql2.parser.GreqlParser;
 import de.uni_koblenz.jgralab.greql2.schema.Comprehension;
 import de.uni_koblenz.jgralab.greql2.schema.ElementRestriction;
+import de.uni_koblenz.jgralab.greql2.schema.ElementTypeRestriction;
+import de.uni_koblenz.jgralab.greql2.schema.ForwardElementSet;
+import de.uni_koblenz.jgralab.greql2.schema.FunctionApplication;
 import de.uni_koblenz.jgralab.greql2.schema.GreqlSyntaxGraph;
 import de.uni_koblenz.jgralab.greql2.schema.IncDirection;
 import de.uni_koblenz.jgralab.greql2.schema.IncidenceDirection;
 import de.uni_koblenz.jgralab.greql2.schema.IncidenceRestriction;
 import de.uni_koblenz.jgralab.greql2.schema.IsConstrainedExpressionOf;
+import de.uni_koblenz.jgralab.greql2.schema.IsIdOfPartialGraphDefinition;
 import de.uni_koblenz.jgralab.greql2.schema.IsIncTypeIdOf;
+import de.uni_koblenz.jgralab.greql2.schema.IsPathOf;
+import de.uni_koblenz.jgralab.greql2.schema.IsStartExprOf;
 import de.uni_koblenz.jgralab.greql2.schema.IsSubgraphDefinitionOf;
+import de.uni_koblenz.jgralab.greql2.schema.IsTypeIdOfRestriction;
 import de.uni_koblenz.jgralab.greql2.schema.KappaSubgraphDefinition;
 import de.uni_koblenz.jgralab.greql2.schema.LocalSubgraphDefinition;
 import de.uni_koblenz.jgralab.greql2.schema.NestedSubgraphDefinition;
@@ -40,19 +47,15 @@ public class DHHTParserTest {
 	}
 
 	@Test
-	public void testSimpleEdgePathDescription() throws Exception {
+	public void testSimpleEdgePathDescription() {
 		GreqlSyntaxGraph graph = parseQuery("using v: v --> ");
 		SimpleEdgePathDescription pathDescr = graph
 				.getFirstSimpleEdgePathDescription();
-		// TODO test seriously
-		// for (Vertex v : graph.vertices()) {
-		// System.out.println("VErtex: " + v);
-		// }
 		assertNotNull(pathDescr);
 	}
 
 	@Test
-	public void testSimpleIncidencePathDescription() throws Exception {
+	public void testSimpleIncidencePathDescription() {
 		GreqlSyntaxGraph graph = parseQuery("using v: v +>");
 		SimpleIncidencePathDescription pathDescr = graph
 				.getFirstSimpleIncidencePathDescription();
@@ -119,21 +122,47 @@ public class DHHTParserTest {
 	}
 
 	@Test
-	public void testSimpleIncidencePathDescriptionWithGoalRestriction()
-			throws Exception {
-		GreqlSyntaxGraph graph = parseQuery("using v: v +> {V:V}");
+	public void testSimpleIncidencePathDescriptionWithGoalRestriction() {
+		GreqlSyntaxGraph graph = parseQuery("getVertex(1) +> {V:V}");
+		ForwardElementSet forwardSet = graph.getFirstForwardElementSet();
+		assertNotNull(forwardSet);
+		FunctionApplication func = graph.getFirstFunctionApplication();
+		assertNotNull(func);
+		IsStartExprOf startOf = graph.getFirstIsStartExprOf();
+		assertNotNull(startOf);
+		assertTrue(startOf.getAlpha() == func);
+		assertTrue(startOf.getOmega() == forwardSet);
+
 		SimpleIncidencePathDescription pathDescr = graph
 				.getFirstSimpleIncidencePathDescription();
 		assertNotNull(pathDescr);
+
+		IsPathOf pathOf = graph.getFirstIsPathOf();
+		assertNotNull(pathOf);
+		assertTrue(pathOf.getAlpha() == pathDescr);
+		assertTrue(pathOf.getOmega() == forwardSet);
+
 		IncidenceDirection incDirection = graph.getFirstIncidenceDirection();
 		assertNotNull(incDirection);
 		assertTrue(incDirection.get_dir() == IncDirection.OUT);
 		ElementRestriction elementRest = graph.getFirstElementRestriction();
 		assertNotNull(elementRest);
+		assertTrue(elementRest instanceof ElementTypeRestriction);
+		IsTypeIdOfRestriction isIdOfRestr = graph
+				.getFirstIsTypeIdOfRestriction();
+		assertNotNull(isIdOfRestr);
+		assertTrue(isIdOfRestr.getOmega() == elementRest);
+		TypeId type = isIdOfRestr.getAlpha();
+		assertNotNull(type);
+		assertTrue(type.get_name().equals("V"));
+		isIdOfRestr = isIdOfRestr.getNextIsTypeIdOfRestriction();
+		assertTrue(isIdOfRestr == null);
+		pathDescr = pathDescr.getNextSimpleIncidencePathDescription();
+		assertTrue(pathDescr == null);
 	}
 
 	@Test
-	public void testSubgraphExpression() throws Exception {
+	public void testSubgraphExpression() {
 		GreqlSyntaxGraph graph = parseQuery("using v: (kappa(1) : v +>)");
 		SubgraphExpression subExpr = graph.getFirstSubgraphExpression();
 		assertNotNull(subExpr);
@@ -191,6 +220,34 @@ public class DHHTParserTest {
 	@Test(expected = ParsingException.class)
 	public void testImplementationExampleExpression() {
 		GreqlSyntaxGraph graph = parseQuery("(partial(1): (kappa(1): v1 -->{EdgeType2} {V:v3} --> +>{Incidence1, Incidence2}+ [<+>{Incidence3}]))");
-		// TODO jtheegarten test for correct creation of SyntaxGraph.
+
+		// Test the partial(1)
+		SubgraphExpression subExpr = graph.getFirstSubgraphExpression();
+		assertNotNull(subExpr);
+		SubgraphDefinition subDef = graph.getFirstSubgraphDefinition();
+		assertNotNull(subDef);
+		assertTrue(subDef instanceof PartialSubgraphDefinition);
+		IsSubgraphDefinitionOf subDefOf = graph
+				.getFirstIsSubgraphDefinitionOf();
+		assertNotNull(subDefOf);
+		assertTrue(subDefOf.getAlpha() == subDef);
+		assertTrue(subDefOf.getOmega() == subExpr);
+		IsIdOfPartialGraphDefinition idOfPartial = graph
+				.getFirstIsIdOfPartialGraphDefinition();
+		assertNotNull(idOfPartial);
+		assertTrue(idOfPartial.getOmega() == subDef);
+		assertTrue(idOfPartial.getAlpha().get_intValue() == 1);
+
+		// Test the kappa(1)
+		subExpr = subExpr.getNextSubgraphExpression();
+		assertNotNull(subExpr);
+		subDef = subDef.getNextSubgraphDefinition();
+		assertNotNull(subDef);
+		assertTrue(subDef instanceof KappaSubgraphDefinition);
+		subDefOf = subDefOf.getNextIsSubgraphDefinitionOf();
+		assertNotNull(subDefOf);
+		assertTrue(subDefOf.getAlpha() == subDef);
+		assertTrue(subDefOf.getOmega() == subExpr);
+		assertTrue(((KappaSubgraphDefinition) subDef).get_kappa() == 1);
 	}
 }
